1711ffb506e9d131f56cd804391da7aca9b08248
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-04-21 22:45:20 +0600

    added mongodb string with env file

--- index.js
+++ index.js
@@ -5,11 +5,41 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan')
 const port = process.env.PORT || 7000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors())
 app.use(morgan('dev'))
+
+
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.3jtn0.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+        app.get('/tourists', async (req, res) => {
+                res.send('hello from tourists route')
+        })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
 
 app.get('', (req,res) => {
         res.send('ExploreEase is running.......')
